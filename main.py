3885cd4fbbe98ce445da0cadd6766995a3abba8a
Ricardo Arias <reariast128@gmail.com>
2023-11-22 18:38:36 -0500

    Added tree, get_last_modified to be used for add_modified_date_to_tree, wich creates a file-tree-like with last modified date with the name of the file of each tree.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 import argparse
+from datetime import datetime
 
 class Backup:
     parser = argparse.ArgumentParser(
@@ -13,26 +14,53 @@ class Backup:
 
     def __init__(self) -> None:
         self.create_arguments()
-        self.args = self.parser.parse_args()
-        self.path = self.args.path
+        self.args: argparse.Namespace = self.parser.parse_args()
+        self.main_path: str = self.args.path
 
     def create_arguments(self) -> None:
         self.parser.add_argument('path', 
                             default="./",
                             help="Indicates the path to work with (default: path where the module is in.)")
 
-    def main(self) -> None:
+    def print_items(self) -> None:
         try:
-            items = os.listdir(self.path)
+            items = os.listdir(self.main_path)
 
-        except FileNotFoundError as error:
-            print(f"ERROR: {error.strerror}")
+        except:
+            print(f"ERROR")
             
         else:
-            for item in items:
-                print(item)
+            files = [file for file in items if path.isfile(file)]
+            directories = [directory for directory in items if path.isdir(directory)]
+
+            print(f"Files: {files}")
+            print(f"Directories: {directories}")
+
+    def tree(self) -> tuple:
+        tree = [item for item in os.walk(self.main_path)]
+        return tree
+    
+    def get_last_modified(self, path) -> datetime:
+        last_modified_seconds = os.path.getmtime(path)
+        last_modified_date = datetime.fromtimestamp(last_modified_seconds)
+        return last_modified_date
+    
+    def add_modified_date_to_tree(self, tree: list[tuple[str, list[str], list[str]]]) -> list[str, tuple[str, datetime]]:
+        tree_w_modified_date = []
+
+        for alloc in tree:
+            path = alloc[0]
+            files = alloc[2]
+            files_with_modified_date = [(filename, self.get_last_modified(path + "\\" + filename)) for filename in files]
+            tree_w_modified_date.append((path, files_with_modified_date))
+
+        return tree_w_modified_date
 
 if __name__ == "__main__":
-    bcp = Backup()
-    bcp.set_path()
-    bcp.main()
+    backup = Backup()
+    tree = backup.tree()
+    mod_tree = backup.add_modified_date_to_tree(tree)
+    for dir in mod_tree:
+        for elem in dir:
+            print(elem)
+        print("\n")
