225546f6e856f3eff90b7acff81dfa3ffac16ace
Ricardo Arias <reariast128@gmail.com>
2023-11-22 18:48:35 -0500

    Added docstrings to all functions and removed os.path dependency, since it's no used in the code.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-from os import path
 import argparse
 from datetime import datetime
 
@@ -18,25 +17,33 @@ class Backup:
         self.main_path: str = self.args.path
 
     def create_arguments(self) -> None:
+        '''A function that initialize the arguments to use in the CLI.'''
         self.parser.add_argument('path', 
                             default="./",
                             help="Indicates the path to work with (default: path where the module is in.)")
 
-    def tree(self) -> tuple:
+    def tree(self) -> list:
+        '''Return a list which is the tree of the main_path directory.'''
         tree = [item for item in os.walk(self.main_path)]
         return tree
     
     def get_last_modified(self, path) -> datetime:
+        '''Get last modified date of a file or directory.'''
         last_modified_seconds = os.path.getmtime(path)
         last_modified_date = datetime.fromtimestamp(last_modified_seconds)
         return last_modified_date
     
     def add_modified_date_to_tree(self, tree: list[tuple[str, list[str], list[str]]]) -> list[str, tuple[str, datetime]]:
+        '''Return a list like provided by tree(), but this doesn't includes the subdirectories of a directory, and each file have the last modified date.'''
         tree_w_modified_date = []
 
-        for alloc in tree:
-            path = alloc[0]
-            files = alloc[2]
+        for directory in tree:
+
+            # Aliasing path and files
+            path = directory[0]
+            files = directory[2]
+
+            # Creating a list with tuples of filename and last modified date of each file
             files_with_modified_date = [(filename, self.get_last_modified(path + "\\" + filename)) for filename in files]
             tree_w_modified_date.append((path, files_with_modified_date))
 
